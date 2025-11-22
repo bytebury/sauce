e08968712444b8bb07d40e90907f64b9a91b2740
Marcello <marchsabino@gmail.com>
2025-11-22 14:07:39 -0500

    feat: fix bytes and how bytes work

--- src/bytes.ts
+++ src/bytes.ts
@@ -4,27 +4,25 @@
  * All functions will return their size in bytes.
  *
  * @example
- * ByteConverter.bytes(10_000); // 10_000
- * ByteConverter.megabytes(5); // 5_242_880
+ * Bytes.bytes(10_000); // 10_000
+ * Bytes.megabytes(5); // 5_242_880
  */
-export const ByteConverter = {
+export const Bytes = {
   /**
    * Converts the bytes to bytes.
    *
    * This just returns what was entered. If a decimal is passed,
    * this will take the ceil.
    */
-  bytes: (b: number): Milliseconds => Math.ceil(b),
+  bytes: (b: number): number => Math.ceil(b),
   /** Converts kilobytes to bytes. */
-  kilobytes: (kb: number): Milliseconds => Math.ceil(kb * 1024),
+  kilobytes: (kb: number): number => Math.ceil(kb * 1024),
   /** Converts megabytes to bytes. */
-  megabytes: (mb: number): Milliseconds => Math.ceil(mb * 1024 ** 2),
+  megabytes: (mb: number): number => Math.ceil(mb * 1024 ** 2),
   /** Converts gigabytes to bytes. */
-  gigabytes: (gb: number): Milliseconds => Math.ceil(gb * 1024 ** 3),
+  gigabytes: (gb: number): number => Math.ceil(gb * 1024 ** 3),
   /** Converts terabytes to bytes. */
-  terabytes: (tb: number): Milliseconds => Math.ceil(tb * 1024 ** 4),
+  terabytes: (tb: number): number => Math.ceil(tb * 1024 ** 4),
   /** Converts petabytes to bytes. */
-  petabytes: (pb: number): Milliseconds => Math.ceil(pb * 1024 ** 5),
+  petabytes: (pb: number): number => Math.ceil(pb * 1024 ** 5),
 };
-
-type Milliseconds = number;
